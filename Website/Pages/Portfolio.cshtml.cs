4e6e1960c1b2fcc7b1f486f17b2d600cdf48596e
Bjørn <bjornsodal@gmail.com>
2025-11-09 15:28:10 +0100

    Basic Page Layout

---
+++ Website/Pages/Portfolio.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages;
+
+public class PortfolioModel : PageModel
+{
+    private readonly ILogger<PortfolioModel> _logger;
+
+    public PortfolioModel(ILogger<PortfolioModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
